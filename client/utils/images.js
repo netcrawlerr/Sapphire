7d701e2d2f17383c578cf0810b25d34c20f239b6
netcrawlerr <abdulmejidamk@gmail.com>
2024-07-25 15:30:35 -0400

    "ignore"

---
+++ client/utils/images.js
@@ -0,0 +1,16 @@
+const images = [
+  {
+    name: "Macbook Pro",
+    url: "../public/mac1.jpg",
+  },
+  {
+    name: "Samsung Galaxy S24",
+    url: "../public/samsung1.jpg",
+  },
+  {
+    name: "Macbook Air",
+    url: "../public/mac2.jpg",
+  },
+];
+
+export default images;
