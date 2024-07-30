5f8865eddbd75e0cb230404a21f47ab9779057f1
abdulmejidamk <abdulmejidamk@gmail.com>
2024-07-30 08:46:20 +0300

    "data"

--- client/utils/images.js
+++ client/utils/images.js
@@ -2,14 +2,62 @@ const images = [
   {
     name: "Macbook Pro",
     url: "/mac1.jpg",
+    description: "An Apple Inc Product to Streamline Your Coding Experience",
+    price: "999",
   },
   {
     name: "Samsung Galaxy S24",
     url: "/samsung1.jpg",
+    description: "Samsung Brought the Latest Tech to yout door",
+    price: "1200",
   },
   {
     name: "Macbook Air",
     url: "/mac2.jpg",
+    description: "Anothe Apple Product",
+    price: "11999",
+  },
+  {
+    name: "Attack On Titan",
+    url: "/aot.jpg",
+    description: "Japanese #1 Selling Anime",
+    price: "17999",
+  },
+  {
+    name: "Macbook Pro",
+    url: "/mac1.jpg",
+    description: "An Apple Inc Product to Streamline Your Coding Experience",
+    price: "999",
+  },
+  {
+    name: "Samsung Galaxy S24",
+    url: "/samsung1.jpg",
+    description: "Samsung Brought the Latest Tech to yout door",
+    price: "1200",
+  },
+  {
+    name: "Macbook Air",
+    url: "/mac2.jpg",
+    description: "Anothe Apple Product",
+    price: "11999",
+  },
+  {
+    name: "Attack On Titan",
+    url: "/aot.jpg",
+    description: "Japanese #1 Selling Anime",
+    price: "17999",
+  },
+  {
+    name: "Macbook Air",
+    url: "/mac2.jpg",
+    description: "Anothe Apple Product",
+    price: "11999",
+  },
+  {
+    name: "Attack On Titan",
+    url: "/aot.jpg",
+    description: "Japanese #1 Selling Anime",
+    price: "17999",
   },
 ];
 
